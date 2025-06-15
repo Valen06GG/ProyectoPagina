69d48c79720477076b0826d9fb87f4c5b5b8c46c
Valentin Fortunato <valenfortunato06@gmail.com>
2025-06-15 17:06:58 -0300

    PrimeraPagina

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('PaginaBlog.urls')),
+    path('PaginaBlog/', include('PaginaBlog.urls')),
 ]
